f2cc0fdcb71aa86fb4b4114d7a3912933a364bf3
formationSquashTF <61993189+formationSquashTF@users.noreply.github.com>
2020-03-10 13:58:44 +0100

    Update AnnotationSteps.java

--- src/test/java/org/cucumber/AnnotationSteps.java
+++ src/test/java/org/cucumber/AnnotationSteps.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
